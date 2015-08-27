062579413bdef5f6ba8a8543a1d6f71e50f4e742
PineAppleTechAustralia <pjeewan@YANGFANs-iMac.local>
2015-08-27 14:04:51 +1000

    GPS enabled
    Includes getting weather using GPS coordinates

--- Pod/WeatherManager.h
+++ Pod/WeatherManager.h
@@ -3,7 +3,7 @@
 //  ToDo
 //
 //  Created by PJ on 8/11/15.
-//  Copyright (c) 2015 PineApple. All rights reserved.
+//  Copyright (c) 2015 PineApple Tech. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
@@ -14,6 +14,6 @@
     @property (strong, nonatomic) NSString *country;
 
     + (WeatherManager *)singleton;
-    - (void)fetchWeather:(void(^)(NSDictionary *info))success failure:(void(^)(NSError *error))failure;
-
+    - (void)fetchWeatherForCity:(void(^)(NSDictionary *info))success failure:(void(^)(NSError *error))failure;
+    - (void)fetchWeatherForLatitude:(double)lat Longitude:(double)lng success:(void(^)(NSDictionary *info))success failure:(void(^)(NSError *error))failure;
 @end
--- Pod/WeatherManager.m
+++ Pod/WeatherManager.m
@@ -3,15 +3,12 @@
 //  ToDo
 //
 //  Created by PJ on 8/11/15.
-//  Copyright (c) 2015 PineApple. All rights reserved.
+//  Copyright (c) 2015 PineApple Tech. All rights reserved.
 //
 
 #import "WeatherManager.h"
 
-//Open Weather Map Key
-
-#define KEY @""
-#define URL @"http://api.openweathermap.org/data/2.5/weather?q=" //{city name},{country code 2 digits}"
+#define URL @"http://api.openweathermap.org/data/2.5/weather?" //{city name},{country code 2 digits}"
 
 @interface WeatherManager ()
 
@@ -36,12 +33,63 @@ float kelvinToCelcius(float kelvin) {
     return kelvin - 273.15;
 }
 
-- (void)fetchWeather:(void(^)(NSDictionary *info))success failure:(void(^)(NSError *error))failure {
+- (void)fetchWeatherForCity:(void(^)(NSDictionary *info))success failure:(void(^)(NSError *error))failure {
     
     NSAssert(self.city != nil, @"City is not set");
     NSAssert(self.country != nil, @"Country is not set");
     
-    NSString *fullUrl = [[NSString alloc] initWithFormat:@"%@%@,%@&APPID=%@", URL, self.city,self.country,KEY];
+    NSString *fullUrl = [[NSString alloc] initWithFormat:@"%@q=%@,%@", URL, self.city,self.country];
+    
+    NSURLSession *session = [NSURLSession sharedSession];
+    [[session dataTaskWithURL:[NSURL URLWithString:fullUrl]
+            completionHandler:^(NSData *data,
+                                NSURLResponse *response,
+                                NSError *error) {
+                
+                NSError *jsonError;
+                NSMutableDictionary *dJSON = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+                
+                if (error != nil) {
+                    if (failure != nil) {
+                        failure(error);
+                    }
+                    
+                    return;
+                }
+                
+                if (jsonError != nil) {
+                    if (failure != nil) {
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            failure(jsonError);
+                        });
+                    }
+                    return;
+                }
+                
+                if (success != nil) {
+                    NSString *description = [dJSON[@"weather"] firstObject][@"description"];
+                    
+                    NSString *min = [[NSString alloc] initWithFormat:@"%.2f",kelvinToCelcius(  [dJSON[@"main"][@"temp_min"] floatValue]  )];
+                    
+                    NSString *max = [[NSString alloc] initWithFormat:@"%.2f",kelvinToCelcius(  [dJSON[@"main"][@"temp_max"] floatValue]  )];
+                    
+                    NSString *current = [[NSString alloc] initWithFormat:@"%.2f",kelvinToCelcius(  [dJSON[@"main"][@"temp"] floatValue]  )];
+                    
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        success(@{@"min": min, @"max": max, @"current": current, @"description": description});
+                    });
+                    
+                }
+                
+            }] resume];
+    
+    
+    
+}
+
+- (void)fetchWeatherForLatitude:(double)lat Longitude:(double)lon success:(void(^)(NSDictionary *info))success failure:(void(^)(NSError *error))failure {
+    
+    NSString *fullUrl = [[NSString alloc] initWithFormat:@"%@lat=%f&lon=%f", URL,lat,lon];
     
     NSURLSession *session = [NSURLSession sharedSession];
     [[session dataTaskWithURL:[NSURL URLWithString:fullUrl]
